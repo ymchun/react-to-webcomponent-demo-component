1c8a8f47bdd6ae127dedb79ff23e25758c49af92
Jean M.C <manchunyip2008@gmail.com>
2022-09-24 00:25:47 +0800

    fix: add component props

--- src/components/poc-component/index.tsx
+++ src/components/poc-component/index.tsx
@@ -5,8 +5,20 @@ import * as ReactDOM from "react-dom/client";
 import reactToWebComponent from "react-to-webcomponent";
 import shadow from "react-shadow";
 
-export const PocComponent: React.FunctionComponent = () => (
-  <h1>This is web component</h1>
+export type Props = {
+  testNumber: number;
+  testString: string;
+};
+
+export const PocComponent: React.FunctionComponent<Props> = ({
+  testNumber,
+  testString,
+}) => (
+  <>
+    <h1>This is web component</h1>
+    <h3>testNumber: {testNumber}</h3>
+    <h3>testString: {testString}</h3>
+  </>
 );
 PocComponent.displayName = "PocComponent";
 
@@ -14,9 +26,9 @@ export function registerPocComponentElement() {
   if (typeof window === "undefined") return;
   if (window.customElements.get("poc-component")) return;
 
-  const WrappedComponent: FunctionComponent = () => (
+  const WrappedComponent: FunctionComponent<Props> = (props) => (
     <shadow.span>
-      <PocComponent />
+      <PocComponent {...props} />
     </shadow.span>
   );
   WrappedComponent.displayName = "WrappedComponent";
@@ -25,7 +37,13 @@ export function registerPocComponentElement() {
   const WebPocComponent = reactToWebComponent(
     WrappedComponent,
     React as never,
-    ReactDOM as never
+    ReactDOM as never,
+    {
+      props: {
+        testNumber: Number,
+        testString: String,
+      },
+    }
   );
   customElements.define("poc-component", WebPocComponent as never);
 }
